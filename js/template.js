dc268de7a08891567522efbb3546857e2e0742db
Nicstradamus <nicholashardy@gmail.com>
2016-12-27 10:25:13 -0800

    Many changes
    Added Name fade in on profile.html via template.js.

--- js/template.js
+++ js/template.js
@@ -1,25 +1,12 @@
 $(document).ready (function() {
 	
-	/*----HIDE WORKS, DURATION DOES NOT--------*/
-	// $("h1").click(function(){
+
 		
-		// $("p").hide(5000);
+		$("h1,h3,p").fadeOut(0);
+		$("h1,h3,p,#selfie").fadeIn(2000);
 	
-	// });
 	
-	/*----TOGGLE WORKS, DURATION DOES NOT--------*/
-	$("h1").mouseenter(function(){
-		
-		$("p").toggle(5000);
 	
-	});
-	
-	/*----FADEIN/FADEOUT DO NOT WORK--------*/
-	$("h1").mouseenter(function(){
-		
-		$("p").fadeIn(5000);
-	
-	});
-	
+
 
 });
---
+++ js/test.js
@@ -0,0 +1,12 @@
+$(document).ready (function() {
+	
+	
+	$("#pic").click(function(){
+		
+		$("#pic").fadeOut(5000);
+	
+	});
+
+	
+
+});
