5acf997aae36a0bc6ad084b2d738a846fdf06f20
Nicstradamus <nicholashardy@gmail.com>
2016-12-29 17:02:03 -0800

    Images, carousel, testing html pages...etc.
    Added new images for carousel

--- js/template.js
+++ js/template.js
@@ -2,8 +2,8 @@ $(document).ready (function() {
 	
 
 		
-		$("h1,h3,p").fadeOut(0);
-		$("h1,h3,p,#selfie").fadeIn(2000);
+		$("h1,h2,h3,h4,p,dl,ul,ol").fadeOut(0);
+		$("h1,h2,h3,h4,p,dl,ul,ol").fadeIn(1000);
 	
 	
 	
