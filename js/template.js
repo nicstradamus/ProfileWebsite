761b6ab0624ca9ad0b58d03dbe5d229f94c074ce
Nicstradamus <nicholashardy@gmail.com>
2017-01-02 20:15:13 -0800

    Added jquery js file and carousel/linc.png to about.html

--- js/template.js
+++ js/template.js
@@ -2,8 +2,8 @@ $(document).ready (function() {
 	
 
 		
-		$("h1,h2,h3,h4,p,dl,ul,ol").fadeOut(0);
-		$("h1,h2,h3,h4,p,dl,ul,ol").fadeIn(1000);
+		$("a,h1,h2,h3,h4,h5,h6,p,dl,ul,ol").fadeOut(0);
+		$("a,h1,h2,h3,h4,h5,h6,p,dl,ul,ol").fadeIn(1000);
 	
 	
 	
